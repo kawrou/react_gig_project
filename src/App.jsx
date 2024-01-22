7b9a535fc39bfb3ffb9f82540e290911dc7ee392
Alan Gardiner <kwarouag@gmail.com>
2024-01-22 17:46:07 +0000

    Created Gig component with basic html

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import Hello from "./components/Hello";
 import makersLogo from "./assets/Makers-Logo.png";
 import "./App.css";
+import React from "react"; 
+import Gig from "./Gig";
 
-function App() {
+
+// function App() {
+//   return 
+//     <Gig />;
+// }
+
+const App = () => {
   return (
     <>
-      <Hello name="World" />
-      <img className="logo" src={makersLogo}></img>
+      {/* <Hello />
+      <img src = {makersLogo} alt = "Makers logo" />  */}
+      <Gig />
     </>
-  );
-}
+  );  
+}; 
+
 
 export default App;
---
+++ src/Gig.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+
+const Gig = () => {
+    return (
+        <div id = "gig-component">
+            <h3 id="band-name">listing name of band</h3>
+            {/* <img src= "" id = "band-image">Image of Band</img> */}
+            <p id ="event-description">Description of event</p>
+            <p id ="date-time">time and date of the event</p>
+            <p id = "location">location</p>
+        </div>
+    );
+};
+
+export default Gig
