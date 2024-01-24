080bec4550c720528347b78db901ddc3e6cbbcfa
Alan Gardiner <kwarouag@gmail.com>
2024-01-24 16:55:10 +0000

    TDD'd a favourite button for Gig component

--- src/components/Gig.jsx
+++ src/components/Gig.jsx
@@ -1,5 +1,5 @@
-import React from "react"
-import "./Gig.css"
+import React, { useState } from "react";
+import "./Gig.css";
 
 // const Gig = () => {
 //     return (
@@ -7,8 +7,8 @@ import "./Gig.css"
 //             <h3 id="band-name">Red Hot Chili Peppers</h3>
 //             <img src= "assets/red_hot_chili_peppers.jpg" id = "band-image" alt = "Red Hot Chili Peppers"></img>
 //             <p id ="event-description">
-//                 The Red Hot Chili Peppers are heading to north London to rock out Tottenham's London Stadium. 
-//                 One of only two UK shows, you don't want to miss this rare chance to see the Chili Peppers IRL. 
+//                 The Red Hot Chili Peppers are heading to north London to rock out Tottenham's London Stadium.
+//                 One of only two UK shows, you don't want to miss this rare chance to see the Chili Peppers IRL.
 //             </p>
 //             <p id ="date-time">July 21 - 4:30</p>
 //             <p id = "location">Tottenham Stadium</p>
@@ -18,15 +18,28 @@ import "./Gig.css"
 
 // USING PROPS
 const Gig = (props) => {
+    const [heartState, setHeartState] = useState(false);
+
+    const toggleHeart = () => {
+        setHeartState(!heartState);
+    };
+
     return (
         <div className="gig-info">
-            <h3 className ="band-name">{props.bandName}</h3>
-            <img src = {props.imageUrl} alt = {props.altText} className = "band-image"/>
-            <p className = "event-description">{props.eventDescription}</p>
-            <p className = "event-date-time">{props.dateTime}</p>
-            <p className = "event-location">{props.location}</p>
+        <h3 className="band-name">{props.bandName}</h3>
+        <img src={props.imageUrl} alt={props.altText} className="band-image" />
+        <p className="event-description">{props.eventDescription}</p>
+        <p className="event-date-time">{props.dateTime}</p>
+        <p className="event-location">{props.location}</p>
+        <button
+            className={heartState ? "heart-filled" : ""}
+            aria-label="favourite button"
+            onClick={toggleHeart}
+        >
+            ♡
+        </button>
         </div>
-    )
-}
+    );
+};
 
-export default Gig
+export default Gig;
---
+++ tests/Gig.test.jsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import "@testing-library/jest-dom";
+import userEvent from "@testing-library/user-event";
+import Gig from "./src/components/Gig"; 
+
+describe("Can render Gig component correctly", () => {
+    test("Renders text elements correctly", () => {
+        render(<Gig 
+            bandName="Red Hot Chili Peppers"
+            eventDescription="Some description"
+            dateTime="July 21 - 4:30 PM"
+            location="Tottenham Stadium"
+        />)
+        expect(screen.getByText("Red Hot Chili Peppers")).toBeInTheDocument(); 
+        expect(screen.getByText("Some description")).toBeInTheDocument();
+        expect(screen.getByText("July 21 - 4:30 PM")).toBeInTheDocument();
+        expect(screen.getByText("Tottenham Stadium")).toBeInTheDocument();
+    })
+    test("Renders image element correctly", () =>{
+        render(<Gig 
+            imageUrl="assets/red_hot_chili_peppers.jpg"
+            altText="Red Hot Chili Peppers band photo" 
+        />)
+        expect(screen.getByAltText("Red Hot Chili Peppers band photo")).toBeInTheDocument(); 
+    })
+    test("Renders favourite button element correctly", () => {
+        render(<Gig/>);
+        expect(screen.getByRole("button")).toBeInTheDocument(); 
+    });
+}); 
+
+describe("Tests for favourite button", () => {
+    test("Heart icon renders filled when user clicks on it", async () => {
+        render(<Gig />)
+
+        expect(screen.getByRole("button")).not.toHaveClass('heart-filled'); 
+    
+        await userEvent.click(screen.getByRole("button")); 
+
+        expect(screen.getByRole("button")).toHaveClass('heart-filled'); 
+    });
+    test("Heart icon renders as empty heart when user clicks on it", async () => {
+        render(<Gig />)
+
+        await userEvent.click(screen.getByRole("button"))
+        expect(screen.getByRole("button")).toHaveClass('heart-filled');
+
+        await userEvent.click(screen.getByRole("button"))
+        expect(screen.getByRole("button")).not.toHaveClass('heart-filled')
+
+    })
+})
